44fdea7560602a42852b267a3240a53783b2d11e
ZionTabrizi1 <82172476+ZionTabrizi1@users.noreply.github.com>
2021-04-19 10:49:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var routes = require("./node_modules/routes");
+var routes = require("./routes");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('./public'));
 app.use(express.static('./public/views'));
+app.use(express.static('./public/views/images'));
+
 app.use(routes);
 
 app.listen(4010, function () {
